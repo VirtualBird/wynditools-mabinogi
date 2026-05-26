5a673f471dcea7c278d7f1776f6635dd0978c99f
SkyPidgeot <33476798+VirtualBird@users.noreply.github.com>
2026-05-26 14:01:27 -0400

    Update CSS styles for home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,15 +7,15 @@ export default function Home(){
             <h1>Home Component</h1>
             <p>Excuse the layout this is still a work in progress some things are liable to break while under construction</p>
             
-            <Link to="wine-making" className="tile-container">
+            <Link to="wine-making" className="tile-container style-btn-cute">
                 Wine Making Calculator
             </Link>
             
-            <Link to="alban-knights-training-stones" className="tile-container">
+            <Link to="alban-knights-training-stones" className="tile-container style-btn-cute">
                 Alban Knight Stone Calculator
             </Link>
 
-            <Link to="cooking" className="tile-container">
+            <Link to="cooking" className="tile-container style-btn-cute">
                 Cooking (under construction)
             </Link>
             <div className="external-links">
